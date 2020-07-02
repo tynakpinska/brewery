d0c50a11d6eaa600e0c7f157e5c519e2820ee1fa
tynakpinska <m.k.kepinska@gmail.com>
2020-07-02 18:23:24 +0200

    add fixed header

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ const links = document.querySelectorAll(".link");
 const exitButton = document.querySelector(".fa-times-circle");
 
 const handleNavClick = () => {
-  nav.style.top = "8px";
+  nav.style.width = "100vw";
+  nav.style.height = "85vh";
   nav.childNodes[1].style.display = "block";
+  nav.childNodes[1].style.position = "absolute";
   links.forEach(l => {
     l.style.width = "93vw";
-    l.style.height = "calc(90vh/6)";
-    l.style.marginBottom = "5px";
+    l.style.height = "calc(70vh/6)";
     l.childNodes[0].style.display = "block";
   });
 };
 const handleExitClick = e => {
   e.stopPropagation();
-  nav.style.left = "10px";
+  nav.style.width = "auto";
+  nav.style.height = "100%";
   nav.childNodes[1].style.display = "none";
   links.forEach(l => {
     l.style.width = "30px";
     l.style.height = "4px";
-    l.style.marginTop = "2px";
-    l.style.marginBottom = "0";
     l.childNodes[0].style.display = "none";
   });
 };
