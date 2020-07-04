c08c9496540606129bf79a8e859f5b45fbed3580
tynakpinska <m.k.kepinska@gmail.com>
2020-07-04 15:09:14 +0200

    fix menu

--- index.js
+++ index.js
@@ -3,26 +3,27 @@ const header = document.getElementsByTagName("header")[0];
 const links = document.querySelectorAll(".link");
 const exitButton = document.querySelector(".fa-times-circle");
 
-const handleNavClick = () => {
+const handleNavClick = e => {
+  console.log(e.target)
   nav.style.width = "100vw";
   nav.style.height = "85vh";
-  nav.childNodes[1].style.display = "block";
-  nav.childNodes[1].style.position = "absolute";
+  nav.children[0].style.display = "block";
+  nav.children[0].style.position = "absolute";
   links.forEach(l => {
     l.style.width = "93vw";
     l.style.height = "calc(70vh/6)";
-    l.childNodes[0].style.display = "block";
+    l.children[0].style.display = "block";
   });
 };
 const handleExitClick = e => {
   e.stopPropagation();
   nav.style.width = "auto";
   nav.style.height = "100%";
-  nav.childNodes[1].style.display = "none";
+  nav.children[0].style.display = "none";
   links.forEach(l => {
     l.style.width = "30px";
     l.style.height = "4px";
-    l.childNodes[0].style.display = "none";
+    l.children[0].style.display = "none";
   });
 };
 
@@ -31,28 +32,57 @@ exitButton.addEventListener("click", handleExitClick);
 links.forEach(l => l.addEventListener("click", handleExitClick));
 
 const controller = new ScrollMagic.Controller();
+
 new ScrollMagic.Scene({
   triggerElement: "#why",
 })
   .setClassToggle("#why", "fade-in")
   .addTo(controller);
+
 new ScrollMagic.Scene({
   triggerElement: "#who",
 })
   .setClassToggle("#who", "fade-in")
   .addTo(controller);
+
 new ScrollMagic.Scene({
   triggerElement: "#what",
 })
   .setClassToggle("#what", "fade-in")
   .addTo(controller);
+
 new ScrollMagic.Scene({
   triggerElement: "#where",
 })
   .setClassToggle("#where", "fade-in")
   .addTo(controller);
+
 new ScrollMagic.Scene({
   triggerElement: "#contact",
 })
   .setClassToggle("#contact", "fade-in")
+  .addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: "#rauchbock",
+})
+  .setClassToggle("#rauchbock", "fade-in")
+  .addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: "#pilsner",
+})
+  .setClassToggle("#pilsner", "fade-in")
+  .addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: "#stout",
+})
+  .setClassToggle("#stout", "fade-in")
+  .addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: "#ale",
+})
+  .setClassToggle("#ale", "fade-in")
   .addTo(controller);
